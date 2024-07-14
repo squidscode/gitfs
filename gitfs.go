8cebfdc84a4ecc1de307a377213a0903f8200643
Idonotcode <siddhantmane@gmail.com>
2024-07-13 21:04:17 -0400

    this commit was automatically committed by gitfs

--- gitfs.go
+++ gitfs.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"os"
-    "os/exec"
-	"path/filepath"
-	"slices"
     "encoding/json"
-	// "strings"
+    "fmt"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "slices"
+    "strings"
 )
 
-/*
- * The driver for the gitfs tool
- */
+type argument_options_t struct {
+    depth int
+    commit_message string
+}
+
+// global state for argument options because they should only be set by the driver
+var argument_options argument_options_t = argument_options_t{
+    depth:5, 
+    commit_message:"this commit was automatically committed by gitfs",
+}
+
 func main() {
     if len(os.Args) != 2 {
         fmt.Fprintf(os.Stderr, "Invalid number of arguments")
@@ -34,7 +42,7 @@ func scanDir(dir string, max_depth int) {
     c, err := os.ReadDir(dir)
     check(err)
 
-    println("Scanning ", dir)
+    println("Scanning", dir)
 
     var subdirs []string
     for _, entry := range c {
@@ -55,39 +63,127 @@ func scanDir(dir string, max_depth int) {
 }
 
 func processDirectory(dir string) {
-    dat := getDefaultGitfsConfig()
-    contents, err := os.ReadFile(filepath.Join(dir, ".gitfs")) 
-    if err != nil {
-        println(".gitfs file found!")
-        if err := json.Unmarshal(contents, &dat); err != nil {
-            panic(err)
-        }
+    config := getDefaultGitfsConfig()
+    gitfs := filepath.Join(dir, ".gitfs")
+    if _, err := os.Stat(gitfs); err == nil {
+        contents, err := os.ReadFile(gitfs)
+        check(err)
+        processGitFsFile(dir, contents, &config)
+    } else {
+        println("No `.gitfs` file found for", dir)
+        println("Resorting to default configuration")
     }
 
-    out, _ := exec.Command("bash", "-c", "cd "+dir+"; git diff").Output()
-    if len(out) == 0 {
-        println("Command on", dir, "exited without any git diff output")
-    } else {
+    git_diff := outputBashInDir(dir, "git diff")
+    if len(git_diff) != 0 && config["autocommit"].(bool) { // stuff to commit found
+        cur_git_branch := strings.TrimSpace(
+            string(outputBashInDir(dir, "git branch --show-current")),
+        )
+        output, err := combinedOutputBashInDir(
+            dir, 
+            fmt.Sprintf(
+                "git stash push; "+
+                "git checkout %s; "+
+                "git stash apply; "+
+                "git add *; "+
+                "git commit -m \"%s\"; "+
+                getPushCommand(&config)+"; "+
+                "git checkout %s; "+
+                "git stash pop",
+                config["branch"],
+                config["commit-message"],
+                cur_git_branch,
+            ),
+        )
+        if err != nil {
+            println(string(output))
+        }
+    } 
+}
 
+func outputBashInDir(dir string, cmd string) []byte {
+    out, err := exec.Command(
+        "bash",
+        "-c",
+        fmt.Sprintf("cd %s; %s", dir, cmd),
+    ).Output()
+    check(err)
+    return out
+}
+
+func combinedOutputBashInDir(dir string, cmd string) ([]byte, error) {
+    return exec.Command(
+        "bash",
+        "-c",
+        fmt.Sprintf("cd %s; %s", dir, cmd),
+    ).CombinedOutput()
+}
+
+func getPushCommand(config *map[string]any) string {
+    if (*config)["autopush"].(bool) {
+        return fmt.Sprintf("git push %s %s", (*config)["remote"], (*config)["branch"])
+    } else {
+        return ":"
+    }
+}
+
+func processGitFsFile(dir string, contents []byte, default_config *map[string]any) {
+    println(".gitfs file found!")
+    addGitFsToGitIgnore(dir)
+    var json_data map[string]interface {}
+    err := json.Unmarshal(contents, &json_data)
+    check(err)
+    for k := range *default_config {
+        if val, exists := json_data[k]; exists {
+            (*default_config)[k] = val
+        }
+    }
+    for k := range json_data {
+        if _, exists := json_data[k]; !exists {
+            // log the error
+            println("[ERROR] key", k, "is not a valid `gitfs` configuration!")
+        }
     }
 }
 
 func getDefaultGitfsConfig() map[string]any {
     return map[string]any {
-        "autocommit": true,
-        "branch": "master",
+        "autocommit": true, // should gitfs autocommit any changes
+        "autopush": false, // should gitfs automatically push if an origin is specified
+        "commit-message": argument_options.commit_message, // the commit message
+        "remote": "origin", // which remote to push to (ie. `git push ????`)
+        "branch": "main", // which branch should be committed to
+    }
+}
+
+func addGitFsToGitIgnore(dir string) {
+    gitignore := filepath.Join(dir, ".gitignore")
+    _, err := os.Stat(gitignore)
+    contents, rerr := os.ReadFile(gitignore)
+    if err == nil { // .gitignore does not exist
+        os.WriteFile(gitignore, 
+            []byte(".gitfs\n"), 
+            0644,
+            )
+    } else if rerr == nil && !strings.Contains(string(contents), ".gitfs\n") { 
+        // gitignore does not contain ".gitfs"
+        os.WriteFile(gitignore, []byte(".gitfs\n"), os.ModeAppend)
+    } else { // unknown error!
+        check(err)
+        check(rerr)
     }
 }
 
 func printHelp() {
     fmt.Printf(
-`gitfs tracks all projects in a root directories and
-auto-commits all changes based on a ".gitfs" config file
+        `gitfs tracks all projects in a root directories and
+        auto-commits all changes based on a ".gitfs" config file
 
-Usage: gitfs DIR_PATH
+        Usage: gitfs ROOT_DIR [-d/--depth DEPTH]
 
-    DIR_PATH - the directory to walk and report git-diffs
-`)
+        ROOT_DIR - the root directory
+        DEPTH - the depth of the walk (default is 5)
+        `)
 }
 
 func check(err error) {
